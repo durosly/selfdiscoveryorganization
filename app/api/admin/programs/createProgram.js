a1dc4b644e904de31114ad0cb6b0e8c41b651d44
durosly <slyboydon1@gmail.com>
2025-01-20 16:58:30 +0100

    📌update: improve response pattern

--- app/api/admin/programs/createProgram.js
+++ app/api/admin/programs/createProgram.js
@@ -1,4 +1,3 @@
-import { NextResponse } from "next/server";
 import ProgramModel, { ProgramValidationSchema } from "@/models/program";
 import connectMongo from "@/lib/connectDB";
 
@@ -9,37 +8,31 @@ async function createProgram(request) {
 		const safe = ProgramValidationSchema.safeParse(body);
 
 		if (!safe.success) {
-			return new Response(
-				JSON.stringify({
+			return Response.json(
+				{
 					status: false,
 					message:
 						safe.error.issues[0].message +
 						" for " +
 						safe.error.issues[0].path[0],
-				}),
+				},
 				{
 					status: 400,
-					headers: {
-						"Content-Type": "application/json",
-					},
 				}
 			);
 		}
 		await connectMongo();
 		await ProgramModel.create(safe.data);
 
-		return NextResponse.json({ status: true, message: "success" });
+		return Response.json({ status: true, message: "success" });
 	} catch (error) {
-		return new Response(
-			JSON.stringify({
+		return Response.json(
+			{
 				status: false,
 				message: error.message,
-			}),
+			},
 			{
 				status: 500,
-				headers: {
-					"Content-Type": "application/json",
-				},
 			}
 		);
 	}
