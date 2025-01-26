962e37d52444e72da72d952ac6b9495208fc88c6
durosly <slyboydon1@gmail.com>
2025-01-26 02:47:20 +0100

    📌fix: update icon error changes

--- app/(admin)/admin/(auth)/layout.jsx
+++ app/(admin)/admin/(auth)/layout.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import {
 	LuCalendarDays,
+	LuChartLine,
 	LuLineChart,
 	LuMail,
 	LuMailbox,
@@ -13,33 +14,25 @@ export const dynamic = "force-dynamic";
 function AdminLayout({ children }) {
 	return (
 		<div className="drawer max-sm:block  lg:drawer-open">
-			<input
-				id="my-drawer-2"
-				type="checkbox"
-				className="drawer-toggle"
-			/>
+			<input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
 			<div className="drawer-content flex flex-col ">
 				{/* Page content here */}
 				<div>
 					<label
 						htmlFor="my-drawer-2"
-						className="btn btn-neutral drawer-button lg:hidden"
-					>
+						className="btn btn-neutral drawer-button lg:hidden">
 						<LuPanelLeftOpen />
 					</label>
 				</div>
 				<main className="p-5">{children}</main>
 			</div>
 			<div className="drawer-side">
-				<label
-					htmlFor="my-drawer-2"
-					className="drawer-overlay"
-				></label>
+				<label htmlFor="my-drawer-2" className="drawer-overlay"></label>
 				<ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
 					{/* Sidebar content here */}
 					<li>
 						<Link href="/admin/dashboard">
-							<LuLineChart />
+							<LuChartLine />
 							Dashbaord
 						</Link>
 					</li>
